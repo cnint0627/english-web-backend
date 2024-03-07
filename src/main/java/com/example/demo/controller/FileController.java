4a4bd1f02a15efb6b53dc123364ed7e053242724
cnint627 <cnint627@hust.edu.cn>
2024-03-07 16:57:15 +0800

    minio文件服务器

--- src/main/java/com/example/demo/controller/FileController.java
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 import com.example.demo.config.MinioConfig;
+import com.example.demo.pojo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,44 +18,54 @@ public class FileController {
 
     // 上传
     @PostMapping("/upload")
-    public Object upload(@RequestParam("file") MultipartFile multipartFile, @RequestParam("bucketName") String bucketName) throws Exception {
-        return this.minioConfig.putObject(multipartFile,bucketName);
+    public Result upload(@RequestParam("file") MultipartFile multipartFile, @RequestParam("bucketName") String bucketName) throws Exception {
+        return Result.success(this.minioConfig.putObject(multipartFile,bucketName));
     }
 
     // 下载文件
     @GetMapping("/download")
-    public void download(String fileName, String bucketName, HttpServletResponse response) {
+    public Result download(String fileName, String bucketName, HttpServletResponse response) {
         this.minioConfig.download(fileName,bucketName,response);
+        return Result.success();
     }
 
     // 列出所有存储桶名称
     @PostMapping("/list")
-    public List<String> list() throws Exception {
-        return this.minioConfig.listBucketNames();
+    public Result list() throws Exception {
+        return Result.success(this.minioConfig.listBucketNames());
     }
 
     // 创建存储桶
     @PostMapping("/createBucket")
-    public boolean createBucket(String bucketName) throws Exception {
-        return this.minioConfig.makeBucket(bucketName);
+    public Result createBucket(String bucketName) throws Exception {
+        if(this.minioConfig.makeBucket(bucketName)){
+            return Result.success();
+        }
+        return Result.error("操作失败");
     }
 
     // 删除存储桶
     @PostMapping("/deleteBucket")
-    public boolean deleteBucket(String bucketName) throws Exception {
-        return this.minioConfig.removeBucket(bucketName);
+    public Result deleteBucket(String bucketName) throws Exception {
+        if(this.minioConfig.removeBucket(bucketName)){
+            return Result.success();
+        }
+        return Result.error("操作失败");
     }
 
     // 列出存储桶中的所有对象名称
     @PostMapping("/listObjectNames")
-    public List<String> listObjectNames(String bucketName) throws Exception {
-        return this.minioConfig.listObjectNames(bucketName);
+    public Result listObjectNames(String bucketName) throws Exception {
+        return Result.success(this.minioConfig.listObjectNames(bucketName));
     }
 
     // 删除一个对象
     @PostMapping("/removeObject")
-    public boolean removeObject(String bucketName, String objectName) throws Exception {
-        return this.minioConfig.removeObject(bucketName, objectName);
+    public Result removeObject(String bucketName, String objectName) throws Exception {
+        if(this.minioConfig.removeObject(bucketName, objectName)){
+            return Result.success();
+        }
+        return Result.error("操作失败");
     }
 }
 
